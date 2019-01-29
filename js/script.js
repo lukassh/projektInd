c010ae34033777115745a9732ad2c4f038fc09de
Lukass <lukassh@gmail.com>
2019-01-29 22:42:10 +0100

    add JS menu

--- js/script.js
+++ js/script.js
@@ -7,3 +7,23 @@ document.querySelector('.icon').addEventListener('click', function(e) {
   e.preventDefault();
   toggleSidebar()
 });
+
+
+
+var details = document.getElementsByTagName('#details'); 
+var links = document.getElementsByTagName('#links'); 
+var banners = document.getElementsByTagName('#banners'); 
+var personal-data = document.getElementsByTagName('#personal-data'); 
+var payout = document.getElementsByTagName('#payout'); 
+var postback = document.getElementsByTagName('#postback'); 
+var main = document.getElementsByTagName('#general-search'); 
+
+details.addEventListener('click', function() {
+	choise.preventDefault();
+	menuSidebar();
+});
+
+function menuSidebar(choise){
+	
+	choise.classList.toggle('active-cell');
+}
