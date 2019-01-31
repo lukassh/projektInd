c443bf4109e49e0ac22ee2bf0e5a15c16de8ab7e
Lukass <lukassh@gmail.com>
2019-01-31 08:43:15 +0100

    modify package.json

--- js/script.js
+++ js/script.js
@@ -82,13 +82,13 @@ function openModal(modal) {
 
 
 // Nav Sidebar
-// Get the container element
+// Get container element
 var btnContainer = document.getElementById("navSidebar");
 
-// Get all buttons with class="btn" inside the container
+// Get all buttons 
 var btns = btnContainer.getElementsByClassName("navChoise");
 
-// Loop through the buttons and add the active class to the current/clicked button
+
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("activenav");
